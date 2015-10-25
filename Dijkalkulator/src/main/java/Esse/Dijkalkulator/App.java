4cc863f180824f5c32e3fc79de633ed857e6bd5b
Esse Zsolt <esse.zs10@gmail.com>
2015-10-25 02:04:17 +0200

    project started

---
+++ Dijkalkulator/src/main/java/Esse/Dijkalkulator/App.java
@@ -0,0 +1,13 @@
+package Esse.Dijkalkulator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
